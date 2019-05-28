2370af5b64081b4cf353ad4175c942583f39dea0
jaesung lim <lim4057@naver.com>
2019-05-28 14:21:42 +0900

    static setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,7 +119,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'.static_root')
-STATIC_DIR = os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS = [
-	STATIC_DIR,
+	os.path.join(BASE_DIR,'blog/static/')
 ]
